22b0c4b0fc3e34d62880240b885a6b4474aa952e
Manon Sella <manonsella@gmail.com>
2024-09-02 22:17:49 +0200

    creates first routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,34 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
+//import App from "./App.jsx";//
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Contact from "./routes/contact";
+
+import Root from "./routes/root";
+import ErrorPage from "./error-page";
+
 import "./index.css";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "contacts/:contactId",
+        element: <Contact />,
+      },
+      {
+        path: "buy",
+        element: <div>Buy our stuff</div>,
+      },
+    ],
+  },
+]);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>
 );
